17c510ac17114457eafc33489f1fd4c99560b9a8
paolo21d <paolo21d@gmail.com>
2019-04-15 21:24:12 +0200

    Fixed Unit Tests

--- src/test/java/pckLocally/MP3PlayerTest.java
+++ src/test/java/pckLocally/MP3PlayerTest.java
@@ -7,6 +7,6 @@ public class MP3PlayerTest {
     @org.junit.Test
     public void getPath() {
         MP3Player player = new MP3Player();
-        assertEquals("C:\\Users\\paolo\\Desktop\\Java Start\\MP3 V2\\src\\sample\\TS22.mp3", player.getPath());
+        assertEquals("C:/Users/paolo/Desktop/Java Start/MP3 V2/src/sample/TS22.mp3", player.getPath());
     }
 }
