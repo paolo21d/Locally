61c35329df2b22679fa2c3a764ca005083c40a92
paolo21d <paolo21d@gmail.com>
2019-04-30 23:28:59 +0200

    Styling with CSS, changed button usage

--- src/main/java/pckLocally/Controller.java
+++ src/main/java/pckLocally/Controller.java
@@ -17,12 +17,11 @@ public class Controller implements Initializable {
     Communication communication = new Communication(player);
     AllPlaylists playlists = new AllPlaylists();
     boolean connection=false;
+    boolean played=false;
 
 
     @FXML
-    private Button playButton;
-    @FXML
-    private Button pauseButton;
+    private Button playPauseButton;
     @FXML
     private Button reloadButton;
     @FXML
@@ -62,37 +61,45 @@ public class Controller implements Initializable {
     }
 
     @FXML
-    void pauseButtonClick(ActionEvent event) {
-        boolean tmp = player.pause();
-        if(tmp)
-            pauseButton.setText("Play");
-        else
-            pauseButton.setText("Pause");
-    }
-
-    @FXML
-    void playButtonClick(ActionEvent event) {
-        labelSongDescription.setText("Opening...");
-        try{
-            player.play();
-        }catch(Exception e){
-            labelSongDescription.setText("File Open Error. Choose right path to file...");
-            return;
+    void playPauseButtonClick(ActionEvent event) {
+        if(!played){ //start play
+            labelSongDescription.setText("Opening...");
+            try{
+                player.play();
+            }catch(Exception e){
+                labelSongDescription.setText("File Open Error. Choose right path to file...");
+                return;
+            }
+            String [] fullPath = player.getPath().split("/");
+            String title = fullPath[fullPath.length-1];
+            labelSongDescription.setText(title);
+            playPauseButton.setText("Pause");
+            played=true;
+        }else { //pause, continue
+            boolean tmp = player.pause();
+            if(tmp)
+                playPauseButton.setText("Play");
+            else
+                playPauseButton.setText("Pause");
         }
-        String [] fullPath = player.getPath().split("/");
-        String title = fullPath[fullPath.length-1];
-        labelSongDescription.setText(title);
-        pauseButton.setText("Pause");
-
-        ///fill table
-        Song song = new Song(title,"-");
-        //TablePlaylist.getItems().add(song);
     }
 
     @FXML
-    void reloadButtonClick(ActionEvent event) {
-        player.reload();
-        pauseButton.setText("Pause");
+    void nextButtonClick(ActionEvent event) {
+        System.out.println("NEXT");
+    }
+
+    @FXML
+    void prevButtonClick(ActionEvent event) {
+        System.out.println("PREV");
+    }
+
+    @FXML
+    void repeatButtonClick(ActionEvent event) {
+        if(played){
+            player.reload();
+            playPauseButton.setText("Pause");
+        }
     }
 
     public void initialize(URL location, ResourceBundle resources) {
