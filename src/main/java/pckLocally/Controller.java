8baaaa7f36a093f062ea4a29ad889547fcf05f80
paolo21d <paolo21d@gmail.com>
2019-05-14 02:50:18 +0200

    Changed text buttons to image button - write handling image buttons

--- src/main/java/pckLocally/Controller.java
+++ src/main/java/pckLocally/Controller.java
@@ -251,5 +251,8 @@ public class Controller implements Initializable {
 
     }
 
+    public void volumeImageClicked(MouseEvent mouseEvent) {
+        //TODO napisac mute, volume, zmiane obrazka wraz ze zmiana volume slidera
+    }
 }
 
